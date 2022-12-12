05e612ee697d4abf0a32483350b037ee31c741d9
Paul Gung <gongshaoxu@gmail.com>
2022-12-12 22:19:34 +0800

    slidev for JDi

---
+++ global-bottom.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer
+      v-if="$slidev.nav.currentPage in [0,1,2,3]"
+      class="absolute top-5 right-5 p-2 "
+  >
+    <img src="/img.png" alt="header" width="160">
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "global-bottom.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ global-top.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <img src="/header-icon.png" alt="header" >
+  </header>
+</template>
+
+<script>
+export default {
+  name: "global-top.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
